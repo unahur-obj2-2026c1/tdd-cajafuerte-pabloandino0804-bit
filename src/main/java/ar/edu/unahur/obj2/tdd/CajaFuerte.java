6bbaa6fd1923861f957d2a9950f819d199800e1b
pabloandino0804-bit <pablo.andino0804@gmail.com>
2026-05-03 17:48:29 -0300

    caja fuerte completado

--- src/main/java/ar/edu/unahur/obj2/tdd/CajaFuerte.java
+++ src/main/java/ar/edu/unahur/obj2/tdd/CajaFuerte.java
@@ -2,7 +2,9 @@ package ar.edu.unahur.obj2.tdd;
 
 public class CajaFuerte {
     private Integer codigo;
+    private Integer limite = 0;
     private Boolean abierta = true;
+    private Boolean bloqueado = false;
 
     public Boolean getEstado(){
         return this.abierta;
@@ -17,21 +19,37 @@ public class CajaFuerte {
     }
 
     public void cerrar(Integer codigo){
-        this.codigo = codigo;
-        this.abierta = false;
-    }
-
-    public Boolean estaCerrada(){
-        return !this.abierta;
+        if (this.estaAbierto()){
+            this.codigo = codigo;
+            this.abierta = false;
+        }
     }
     
     public void abrir(Integer codigoPuesto){
-        if (this.codigo.equals(codigoPuesto)){
+        if (this.codigoCorrecto(codigoPuesto)){
             this.abierta = true;
+        }
+        else if(!this.estaBloqueado()){
+            this.limite +=1;
+            if(limite == 3){
+            this.bloqueado = true;
+            }
         }
     }
 
-    public Boolean estaAbierta(){
-        return this.abierta;
+    public Boolean estaAbierto(){
+        return this.abierta.equals(true);
+    }
+
+    public Boolean codigoCorrecto(Integer unCodigo){
+        return this.codigo.equals(unCodigo);
+    }
+
+    public Boolean estaCerrado(){
+        return !this.estaAbierto();
+    }
+
+    public Boolean estaBloqueado(){
+        return this.bloqueado.equals(true);
     }
 }
--- src/test/java/ar/edu/unahur/obj2/tdd/CajaFuerteTest.java
+++ src/test/java/ar/edu/unahur/obj2/tdd/CajaFuerteTest.java
@@ -5,34 +5,61 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import org.junit.jupiter.api.Test;
 
 public class CajaFuerteTest {
-    
+    /*Test 1 */
     @Test
     public void queLaCajaFuerteEsteAbiertaAlCrearse(){
         CajaFuerte caja = new CajaFuerte();
-        assertTrue(caja.estaAbierta());
+        assertTrue(caja.estaAbierto());
     }
 
+    /*Test 2 */
     @Test
     public void queSePuedeCerrarLaCajaFuerteConCodigo(){
         CajaFuerte caja = new CajaFuerte();
         caja.cerrar(1234);
-        assertFalse(caja.estaAbierta());
-        assertTrue(caja.estaCerrada());
+        assertFalse(caja.estaAbierto());
+        assertTrue(caja.estaCerrado());
     }
 
+    /*Test 3 */
     @Test
     public void queSePuedeAbrirLaCajaFuerteConCodigo(){
         CajaFuerte caja = new CajaFuerte();
         caja.cerrar(1234);
         caja.abrir(1234);
-        assertTrue(caja.estaAbierta());
+        assertTrue(caja.estaAbierto());
     }
 
+    /*Test 4 */
     @Test
     public void noSePuedeAbrirLaCajaFuerteCuandoElCodigoEsIncorrecto(){
         CajaFuerte caja = new CajaFuerte();
         caja.cerrar(1234);
         caja.abrir(4321);
-        assertFalse(caja.estaAbierta());
+        assertFalse(caja.estaAbierto());
+    }
+
+    /*Test 5 */
+    @Test
+    public void seSigaCerradoConMismoCodigo(){
+        CajaFuerte caja = new CajaFuerte();
+        caja.cerrar(1234);
+        caja.cerrar(2345);
+        caja.abrir(1234);
+        assertTrue(caja.estaAbierto());
+    }
+
+    /*
+    Test 6:
+    Al errar 3 veces para abrir la caja fuerte se bloqueara la caja en si.
+    */
+    @Test
+    public void laCajaSeBloqueAlErrar3Veces(){
+        CajaFuerte caja = new CajaFuerte();
+        caja.cerrar(1234);
+        caja.abrir(2345);
+        caja.abrir(2345);
+        caja.abrir(2345);
+        assertTrue(caja.estaBloqueado());
     }
 }
